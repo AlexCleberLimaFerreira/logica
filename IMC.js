b42b8285d93f558f59c099647b8b24be5b6bb671
AlexCleberLimaFerreira <alexcleberlimaferreira@gmail.com>
2025-04-24 16:23:21 -0300

    for e array

---
+++ IMC.js
@@ -0,0 +1,6 @@
+let peso= parseint(readline.question("Qual seu peso?"));
+let altura= Number(readline.question("Você é estudante ou idoso? (s/n)"));
+
+let IMC= peso/(altura*altura);
+
+
--- cinema.js
+++ cinema.js
@@ -1,7 +1,7 @@
 // no javascript variavel se escreve "let" e não "var"              &&= "E"       ||="OU"
 
 
-const readline = require("readline-sync");
+const readline = required("readline-sync");
 
 let idade =  parseInt(readline.question("Qual sua idade? "));
 let carteira= readline.question("Você é estudante ou idoso? (s/n) "); 
@@ -11,6 +11,8 @@ if (idade<16) {console.log("não pode entrar")}
  else if(idade<18 || idade>=65 || carteira=="s") {
     console.log("paga meia entrada")
  }
+
+// else NÃO possui condição.
 
  else{
     console.log("inteiro")
